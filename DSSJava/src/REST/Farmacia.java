ca4436c5b9ad88bb48a314f81d5266674156b9f4
adritake <atr_96@hotmail.com>
2018-12-29 10:17:05 +0100

    Incluyo funcionalidad para hacer un GET simple al servlet

--- DSSJava/src/REST/Farmacia.java
+++ DSSJava/src/REST/Farmacia.java
@@ -1,5 +1,21 @@
 package REST;
 
-public class Farmacia {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Farmacia extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+        
+        request.getRequestDispatcher("/WEB-INF/ListaFarmacias.html").forward(request, response);
+
+	}
 
 }
--- DSSJava/src/REST/Usuarios.java
+++ DSSJava/src/REST/Usuarios.java
@@ -1,5 +1,20 @@
 package REST;
 
-public class Usuarios {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Usuarios extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+        
+        request.getRequestDispatcher("/WEB-INF/ListaUsuarios.html").forward(request, response);
+
+	}
+	
 }
